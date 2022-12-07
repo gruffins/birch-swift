6cb3941b253f278e70c70d84cd12f4ebee905a9e
Ryan Fung <381273+gruffins@users.noreply.github.com>
2022-12-07 14:00:44 -0500

    Add SPM (#6)

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "Birch",
+    products: [
+        .library(
+            name: "Birch",
+            targets: ["Birch"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Birch",
+            dependencies: []),
+    ]
+)
