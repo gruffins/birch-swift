05c229dd14b11b039fcb93dd84cabd18d976ff3e
Ryan Fung <ryanh.fung@gmail.com>
2024-01-04 15:01:47 -0500

    Fix test deprecations

--- Tests/Helpers/TestSession.swift
+++ Tests/Helpers/TestSession.swift
@@ -9,14 +9,6 @@ import Foundation
 
 @testable import Birch
 
-class TestURLSessionDataTask: URLSessionDataTask {
-    override func resume() {}
-}
-
-class TestURLSessionUploadTask: URLSessionUploadTask {
-    override func resume() {}
-}
-
 class TestSession: SessionProtocol {
     var statusCode = 200
     var responseBody = "{}"
@@ -25,13 +17,31 @@ class TestSession: SessionProtocol {
         let response = HTTPURLResponse(url: request.url!, statusCode: statusCode, httpVersion: "1.0", headerFields: nil)
         let data = responseBody.data(using: .utf8)!
         completionHandler(data, response, nil)
-        return TestURLSessionDataTask()
+        
+        return URLSession.shared.dataTask(with: clone(request)!)
     }
 
     func uploadTask(with request: URLRequest, from bodyData: Data?, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionUploadTask {
         let response = HTTPURLResponse(url: request.url!, statusCode: statusCode, httpVersion: "1.0", headerFields: nil)
         let data = responseBody.data(using: .utf8)!
         completionHandler(data, response, nil)
-        return TestURLSessionUploadTask()
+        
+        return URLSession.shared.uploadTask(with: clone(request)!, from: bodyData!)
+    }
+    
+    func clone(_ request: URLRequest) -> URLRequest? {
+        guard var comps = URLComponents(url: request.url!, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+        
+        comps.host = "localhost"
+        
+        if let url = comps.url {
+            var clone = request
+            clone.url = url
+            return clone
+        } else {
+            return nil
+        }
     }
 }
